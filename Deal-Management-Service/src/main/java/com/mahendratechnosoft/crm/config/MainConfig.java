c80280d1282849d0ccc5a2343d22449ccc7cc53f
Mahendra Nikam <info@mahendratechnosoft.com>
2025-11-18 16:01:12 +0530

    Update MainConfig.java

--- Deal-Management-Service/src/main/java/com/mahendratechnosoft/crm/config/MainConfig.java
+++ Deal-Management-Service/src/main/java/com/mahendratechnosoft/crm/config/MainConfig.java
@@ -66,7 +66,7 @@ public class MainConfig {
                 .requestMatchers("/admin/**","/lead/**").hasRole("ADMIN")
                 .requestMatchers("/employee/**").hasRole("EMPLOYEE")
                 .requestMatchers("/super/**").hasRole("SUPERADMIN")
-                .requestMatchers("/signin", "/send_otp", "/verify_otp", "/change_password","/register","/registerData","/checkEmail/**","/generateLeads").permitAll()
+                .requestMatchers("/signin", "/send_otp", "/verify_otp", "/change_password","/register","/registerData","/checkEmail/**","/generateLeads","/createDonor").permitAll()
 
                 .anyRequest().authenticated()
                 .and()
