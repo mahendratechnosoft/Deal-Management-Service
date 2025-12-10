7bea86101e8d344a5f92d9f53bd3b634b32cf889
theteritesh <theteritesh2@gmail.com>
2025-12-10 17:13:59 +0530

    now task is visible follower and assignee

--- Deal-Management-Service/src/main/java/com/mahendratechnosoft/crm/controller/AdminController.java
+++ Deal-Management-Service/src/main/java/com/mahendratechnosoft/crm/controller/AdminController.java
@@ -1127,9 +1127,10 @@ public class AdminController {
 	
 	@GetMapping("/getAllTaskList/{page}/{size}")
 	public ResponseEntity<?> getAllTaskList(@ModelAttribute("admin") Admin admin, @PathVariable int page,@PathVariable int size,
-			@RequestParam(required = false) String search) {
+			@RequestParam(required = false) String search,
+			@RequestParam(required = false)TaskStatus status) {
 
-		return taskService.getAllTaskList(page ,size,admin,search);
+		return taskService.getAllTaskList(page ,size,admin,search,status);
 
 	}
 	
@@ -1148,7 +1149,7 @@ public class AdminController {
 	
 	@GetMapping("/getTaskByItemId/{taskId}")
 	public ResponseEntity<?> getTaskByItemId(@PathVariable String taskId) {
-		return taskService.getTaskById(taskId);	
+		return taskService.getTaskById(taskId,null);	
 	}
 	
 	@DeleteMapping("/deleteTask/{taskId}")
