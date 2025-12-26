2c1dba9514472322d10869a7f7174c0fec46b9d2
Mahendra Nikam <info@mahendratechnosoft.com>
2025-12-26 17:26:50 +0530

    Update WebSocket endpoint allowed origins

--- Deal-Management-Service/src/main/java/com/mahendratechnosoft/crm/config/WebSocketConfig.java
+++ Deal-Management-Service/src/main/java/com/mahendratechnosoft/crm/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173")
+                .setAllowedOrigins("https://mtechnosoft.xpertbizsolutions.com")
                 .withSockJS();
     }
 
